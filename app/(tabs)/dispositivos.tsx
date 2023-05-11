c3725a73d1c0d0dc2a76e8f3cbea0fe17a0facd7
João Ferreira <74504877+joaopferreira01@users.noreply.github.com>
2023-05-12 00:21:03 +0100

    Update dispositivos

--- app/(tabs)/dispositivos.tsx
+++ app/(tabs)/dispositivos.tsx
@@ -35,7 +35,7 @@ export default function DispositivosScreen() {
       {/* Este vai ser o container do nome do dispositivo e do botao remover */}
       <View style={styles.dispositivos}>
         {dispositivos.map((dispositivo, index) => (
-          <Text key={index} style={styles.id}> {dispositivo.tipo}
+          <Text key={index}> {dispositivo.tipo}
             <View>
             <Text style={styles.remover} onPress={() => handleRemoveButtonPress(index)}>Remover</Text>
             </View>
@@ -68,7 +68,7 @@ export default function DispositivosScreen() {
 
             <DropDownPicker
               items={[
-                { label: 'Relógio', value: 'Relógio' },
+                { label: 'Relógio ', value: 'Relógio ' },
                 { label: 'Pulseira', value: 'Pulseira' },
               ]}
               placeholder="Selecione o tipo de dispositivo"
@@ -161,21 +161,17 @@ const styles = StyleSheet.create({
   remover: {
     backgroundColor: 'red',
     borderRadius: 35,
-    marginLeft: 100,
     color: 'white',
     width: 90,
     textAlign: 'center',
     lineHeight: 40,
-  },
-  id: {
-    fontWeight: 'bold',
-    color: 'black',
-    height: 50,
     marginLeft: 100,
-
   },
   dispositivos: {
+    color: 'black',
+    marginLeft: 100,
     marginRight: '40%',
+    marginTop: 100,
   },
   addButtonLabel: {
     color: '#fff',
